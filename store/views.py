fbd793d8e2733ceb8ba4a4f5a04747716e1da8c9
DucLeTrong <trongduclebk@gmail.com>
2021-06-25 14:54:45 +0700

    Add view in store

--- store/views.py
+++ store/views.py
@@ -7,10 +7,23 @@ def store(request, category_slug=None):
 
     if category_slug != None:
         category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.all().filter(category=category,is_available=True)
+        products = Product.objects.all().filter(category=category, is_available=True)
     else:
         products = Product.objects.all().filter(is_available=True)
     context = {
         "products" : products
     }
     return render(request, "store/store.html", context)
+
+def product_detail(request, category_slug = None, product_slug=None):
+
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {
+        "single_product" : single_product
+    }
+
+    return render(request, "store/product_detail.html", context)
