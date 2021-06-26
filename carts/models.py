39184dd07c86480cc2ea28f04fa584970246e0a0
DucLeTrong <trongduclebk@gmail.com>
2021-06-26 10:36:36 +0700

    Fix fontawesome and add cart feature

--- carts/models.py
+++ carts/models.py
@@ -6,7 +6,6 @@ class Cart(models.Model):
     cart_id      = models.CharField(max_length=250, blank=True)
     date_added   = models.DateField(auto_now_add=True)
 
-
     def __str__(self) -> str:
         return self.cart_id
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('store/', include("store.urls")),
     path('cart/', include("carts.urls")),
    
-] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
