784e917d0ab21313f74c41da7ffae557eb510d46
DucLeTrong <trongduclebk@gmail.com>
2021-06-30 11:37:11 +0700

    Fix store display item bugs

--- store/views.py
+++ store/views.py
@@ -12,20 +12,28 @@ from carts.models import CartItem
 # Create your views here.
 
 def store(request, category_slug=None):
+    categories = None
+    products = None
 
     if category_slug != None:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.all().filter(category=category, is_available=True)
-    else:
-        products = Product.objects.all().filter(is_available=True).order_by("id")
-        number_products = len(products)
-        paginator = Paginator(products, 6)
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
 
-    
-    
-    return render(request, "store/store.html", context)
+    context = {
+        'products': paged_products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
 
 def product_detail(request, category_slug = None, product_slug=None):
 
