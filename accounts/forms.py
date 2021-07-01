cb12be178c44c50d7768aa1e6566809db9b07d6f
DucLeTrong <trongduclebk@gmail.com>
2021-07-01 23:30:52 +0700

    Add registration form

---
+++ accounts/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Enter Password",
+        'class': 'form-control',
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Confirm Password"
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs ):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter Email Address'
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,10 +21,12 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include("store.urls")),
     path('cart/', include("carts.urls")),
-   
+    path('accounts/', include("accounts.urls")),
+
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
 + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
