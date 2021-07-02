0050ba79ed27a82c8bfdf9be336a5a88cddbf5dc
DucLeTrong <trongduclebk@gmail.com>
2021-07-02 10:00:18 +0700

    Finish register process

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,10 +18,20 @@ class RegistrationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs ):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
-        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last name'
-        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter Email Address'
 
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get("confirm_password")
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
