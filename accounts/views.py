9f9cae2ad65b9048b393bcb2549b66f4aff97710
DucLeTrong <trongduclebk@gmail.com>
2021-07-05 08:50:44 +0700

    Assign the user to cart item

--- accounts/views.py
+++ accounts/views.py
@@ -15,6 +15,9 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage 
 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+
 # Create your views here.
 def register(request):
     if request.method == "POST":
@@ -69,6 +72,19 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try: 
+                # print(_cart_id(request))
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+
+                if is_cart_item_exists:
+                    card_item = CartItem.objects.filter(cart=cart)
+
+                    for item in card_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, "You are now logged in.")
             # return redirect('home')
