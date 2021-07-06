bb1d8df755cfcb6244ddab602f8f24000caf806e
DucLeTrong <trongduclebk@gmail.com>
2021-07-06 14:03:43 +0700

    Setup order page

--- orders/views.py
+++ orders/views.py
@@ -9,6 +9,11 @@ from .forms import OrderForm
 from .models import Order
 
 # Create your views here.
+
+def payments(request):
+    return render(request, 'orders/payments.html')
+
+
 def place_order(request, total = 0, quantity = 0):
     current_user = request.user
 
@@ -49,20 +54,33 @@ def place_order(request, total = 0, quantity = 0):
             data.tax = tax
             data.ip = request.META.get("REMOTE_ADDR")
 
-            # data.save()
+            data.save()
 
             # Generate order number
             yr = int(datetime.date.today().strftime('%Y'))
             dt = int(datetime.date.today().strftime('%d'))
             mt = int(datetime.date.today().strftime('%m'))
 
+
             d = datetime.date(yr, mt, dt)
 
             current_date = d.strftime("%Y%m%d")
             order_number = current_date + str(data.id)
+            # time_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            # order_number = time_now + str(data.id)
             print(order_number)
             data.order_number = order_number
             data.save()
-            return redirect('checkout')
 
+            order = Order.objects.get(user=current_user,
+                                     is_ordered=False,
+                                    order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+            return render(request, 'orders/payments.html', context)
     return redirect('checkout')
